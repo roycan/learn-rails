b59fa1c1d3b14b43f0ce802edf8c36aac8654a66
roycan <>
2021-05-15 12:06:54 +0000

    start railstutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  root to: 'visitors#new'
-  get '/about', to: 'pages#about'
-  
-  resources( :contacts, :only => [:new, :create])
 end
