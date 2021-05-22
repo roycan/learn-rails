e6b561b353a0c2de20157d1d1190dda636ed55ac
roycan <>
2021-05-22 08:59:00 +0000

    finsih layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
   
-  
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
+  get('/signup', to: "users#new") 
+  get '/help', to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
   get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,16 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  include ApplicationHelper
+
+  setup do 
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select("title", full_title("Sign up"))
+  end
+end
